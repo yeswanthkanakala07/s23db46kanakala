db9b79e64cce3b30e24c583e68e442ec20fec802
yeswanthkanakala07 <82636668+yeswanthkanakala07@users.noreply.github.com>
2023-11-09 18:40:49 -0600

    Update api.js

--- controllers/api.js
+++ controllers/api.js
@@ -1,7 +1,7 @@
 // API for our resources
 exports.api = function(req, res) {
     res.write('[');
-    res.write('{"resource":"fruit", ');
+    res.write('{"resource":"fruits", ');
     res.write(' "verbs":["GET","PUT", "DELETE"] ');
     res.write('}');
     res.write(']')
