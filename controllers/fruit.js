3d40e0e2f45e35a8853e9c10506642940edb65a0
yeswanthkanakala07 <82636668+yeswanthkanakala07@users.noreply.github.com>
2023-11-15 15:43:33 -0600

    Updated fruitupdate.pug

--- controllers/fruit.js
+++ controllers/fruit.js
@@ -121,4 +121,18 @@ exports.fruit_create_Page = function(req, res) {
    };
 
 
+   // Handle building the view for updating a costume.
+// query provides the id
+exports.fruit_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await fruit.findById(req.query.id)
+    res.render('fruitupdate', { title: 'Fruit Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
     
--- routes/fruits.js
+++ routes/fruits.js
@@ -8,4 +8,8 @@ router.get('/', fruit_controller.fruit_view_all_Page);
 router.get('/detail', fruit_controller.fruit_view_one_Page);
 /* GET create costume page */
 router.get('/create', fruit_controller.fruit_create_Page);
+/* GET create update page */
+router.get('/update', fruit_controller.fruit_update_Page);
+/* GET delete costume page */
+router.get('/delete', fruit_controller.fruit_delete_Page);
 module.exports = router;
